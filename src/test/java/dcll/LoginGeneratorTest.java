8ad86b5328e682777b36f512ae1539bc4c0e43c7
rottanaly <rottanaly@ymail.com>
2016-04-15 15:09:51 +0200

    test unitaire LoginGeneratorTest

--- src/test/java/dcll/LoginGeneratorTest.java
+++ src/test/java/dcll/LoginGeneratorTest.java
@@ -15,25 +15,21 @@ public class LoginGeneratorTest {
 
     @Test
     public void testGenerateLoginForNomAndPrenom1() throws Exception {
-        String login = loginGenerator.generateLoginForNomAndPrenom("Paul", "Durand");
-        boolean testLogin = loginService.loginExists(login);
-        assertTrue(testLogin);
+        String login = loginGenerator.generateLoginForNomAndPrenom("Durand", "Paul");
+        assertEquals(login, "PDUR");
 
     }
 
     @Test
     public void testGenerateLoginForNomAndPrenom2() throws Exception {
-        String login = loginGenerator.generateLoginForNomAndPrenom("Jean", "Rolling");
-        boolean testLogin = loginService.loginExists(login);
-        assertTrue(testLogin);
+        String login = loginGenerator.generateLoginForNomAndPrenom("Rolling", "Jean");
+        assertEquals(login, "JROL1");
 
     }
 
     @Test
     public void testGenerateLoginForNomAndPrenom3() throws Exception {
-        String login = loginGenerator.generateLoginForNomAndPrenom("Paul", "Dùrand");
-        boolean testLogin = loginService.loginExists(login);
-        assertTrue(testLogin);
-
+        String login = loginGenerator.generateLoginForNomAndPrenom("Dùrand", "Paul");
+        assertEquals(login, "PDUR");
     }
 }
