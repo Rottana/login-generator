02719201ea7c80f4ba4e6afe79a02a25c7314027
rottanaly <rottanaly@ymail.com>
2016-04-15 14:24:18 +0200

    modifications du code

--- src/main/java/dcll/LoginGenerator.java
+++ src/main/java/dcll/LoginGenerator.java
@@ -4,24 +4,30 @@ import java.text.Normalizer;
 import java.util.regex.Pattern;
 
 /**
- * Classe representant un generateur de login
+ * Classe representant un generateur de login.
  */
 public class LoginGenerator {
 
+    /**
+     * LoginService.
+     */
     private LoginService loginService;
 
     /**
-     * Construit un login generator
+     * Construit un login generator.
      * @param loginService le service de login
      */
-    public LoginGenerator(LoginService loginService) {
+    public LoginGenerator(final LoginService loginService) {
         this.loginService = loginService;
     }
 
     /**
-     * Genere un login unique a partir d'un nom et d'un prenom en prenant la premiere lettre du prenom, concatenee avec
-     * les 3 premieres lettres du nom, le tout mis en lettres capitales et desaccentue. Le login genere doit etre unique
-     * par rapport a la liste des logins existants. En cas de doublon, on incremente le doublon d'un indice. Ci dessous des
+     * Genere un login unique a partir d'un nom et d'un prenom
+     * en prenant la premiere lettre du prenom, concatenee avec
+     * les 3 premieres lettres du nom, le tout mis en lettres capitales
+     * et desaccentue. Le login genere doit etre unique
+     * par rapport a la liste des logins existants. En cas de doublon,
+     * on incremente le doublon d'un indice. Ci dessous des
      * exemples :
      * <ul>
      *     <li>Paul Dupond -> PDUP </li>
@@ -33,25 +39,28 @@ public class LoginGenerator {
      * @param prenom le prenom
      * @return le login genere
      */
-    public String generateLoginForNomAndPrenom(String nom, String prenom) {
-        String p = deAccent(prenom.substring(0,1).toUpperCase());
-        String n = deAccent(nom.substring(0,3).toUpperCase());
-        String login = p+n ;
+    public final String generateLoginForNomAndPrenom(
+            final String nom, final String prenom) {
+        final int nomLongueur = 3;
+        String p = deAccent(prenom.substring(0, 1).toUpperCase());
+        String n = deAccent(nom.substring(0, nomLongueur).toUpperCase());
+        String login = p + n;
         if (loginService.loginExists(login)) {
-            login = login + "1" ;
+            login = login + "1";
         }
         loginService.addLogin(login);
         return login;
     }
 
     /**
-     * Supprime les accents d'une chaine de caractere
+     * Supprime les accents d'une chaine de caractere.
      *
      * @param str la chaine de caractere
      * @return la chaine de caractere sans accents
      */
-    private String deAccent(String str) {
-        String nfdNormalizedString = Normalizer.normalize(str, Normalizer.Form.NFD);
+    private String deAccent(final String str) {
+        String nfdNormalizedString = Normalizer.normalize(
+                str, Normalizer.Form.NFD);
         Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
         return pattern.matcher(nfdNormalizedString).replaceAll("");
     }
---
+++ src/main/java/dcll/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by rottanaly on 4/15/16.
+ */
+package dcll;
